92b96e5b5bfc504ce64f067f2b9dff132613a5b4
SEDict <huangliangzhang@sina.com>
2018-12-26 18:18:37 +0800

    2018-12-26

--- seDictionary.py
+++ seDictionary.py
@@ -36,12 +36,12 @@ class MultiDict:
 # zd=MultiDict("APIdata_",4)
 with open("APIdata_0.json") as fr:
 	zd1=json.loads(fr.read())
-with open("APIdata_1.json") as fr:
-	zd2=json.loads(fr.read())
-with open("APIdata_2.json") as fr:
-	zd3=json.loads(fr.read())
-with open("APIdata_3.json") as fr:
-	zd4=json.loads(fr.read())
+# with open("APIdata_1.json") as fr:
+# 	zd2=json.loads(fr.read())
+# with open("APIdata_2.json") as fr:
+# 	zd3=json.loads(fr.read())
+# with open("APIdata_3.json") as fr:
+# 	zd4=json.loads(fr.read())
 #TMP
 f = open("abbreviationSynonym.json")
 synonym_dic = json.loads(f.read())
